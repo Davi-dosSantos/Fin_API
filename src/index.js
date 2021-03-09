91ddda26f440f75fd17b82e2a3e17a032c4c447b
Davi Costa <daviita1@hotmail.com>
2021-03-08 22:42:23 -0300

    vrify create existent account add

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ const { v4: uuidv4 } = require("uuid")
 
 const app = express();
 
+const customer = [];
+
 app.use(express.json());
 
-app.post("/account", (req, res) => {
+
+app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
+    const customerAlreadyExists = customer.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: 'Customer already exists!' });
+    }
+
+    customer.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send();
 })
-//Localhost:3333
+
 app.listen(3333);
